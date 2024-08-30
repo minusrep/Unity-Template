15eaf4515bd165747cbf8cf369c63d169dd535a5
Maxim S. <its3zin.main@gmail.com>
2024-08-31 00:43:28 +0300

    feat(project): add assets provider

--- Template/Assets/Root/_Scripts/_Constants/AssetsConstants.cs
+++ Template/Assets/Root/_Scripts/_Constants/AssetsConstants.cs
@@ -1,7 +1,31 @@
-﻿namespace Root._Constants
+﻿using UnityEngine;
+
+namespace Root._Constants
 {
     public static class AssetsConstants
     {
+        public static T Load<T>(string path) where T : Object
+        {
+            var founded = Resources.Load<T>(path);
 
+            if (founded == null) LogEmpty(path);
+
+            return founded;
+        }
+
+        public static T[] LoadAll<T>(string path) where T : Object
+        {
+            var founded = Resources.LoadAll<T>(path);
+
+            if (founded == null) LogEmpty(path);
+
+            return founded;
+        }    
+
+
+        private static void LogEmpty(string path)
+        {
+            Debug.Log($"{path} not found");
+        }
     }
 }
