9d2391f3bdbe87b57360326660d77b97421a424b
Maxim S. <its3zin.main@gmail.com>
2024-09-04 02:14:54 +0300

    feat(project): test

--- Template/Assets/Root/_Scripts/Services/UI/Elements/Windows/_Core/UIWindow.cs
+++ Template/Assets/Root/_Scripts/Services/UI/Elements/Windows/_Core/UIWindow.cs
@@ -8,6 +8,7 @@ namespace Root.Services.UI.Elements.Windows
 {
     public abstract class UIWindow : UIElement<VisualElement>
     {
+        // comment
         public abstract string Name { get; }
 
         protected UIDocument _document;
